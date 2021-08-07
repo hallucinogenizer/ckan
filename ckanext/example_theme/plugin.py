1d430966a93c794711da9975ec3e63d13198072d
hallucinogenizer <22100063@lums.edu.pk>
2021-08-07 18:33:12 +0500

    created helper function and snippet

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -2,12 +2,17 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+def newest_datasets():
+    datasets = toolkit.get_action('package_search')(data_dict={'sort':'metadata_modified desc','rows':4})
+    return datasets
+
 class ExampleThemePlugin(plugins.SingletonPlugin):
     '''An example theme plugin.
 
     '''
     # Declare that this class implements IConfigurer.
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config):
 
@@ -18,3 +23,6 @@ class ExampleThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fantastic', 'example_theme')
+
+    def get_helpers(self):
+        return {'example_theme_newest_datasets':newest_datasets}
