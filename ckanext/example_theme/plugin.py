fb159110e710bc9ffa3e2391c5ed414d82b5c14e
hallucinogenizer <22100063@lums.edu.pk>
2021-08-19 19:35:09 +0500

    updating dataset schema to include a category field

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -37,3 +37,48 @@ class DatasetCategoriesPlugin(plugins.SingletonPlugin):
 
     def update_config(self, config):
         pass
+
+    def dataset_facets(self, facets_dict, package_type):
+        facets_dict['categories'] = toolkit._('Categories')
+        return facets_dict
+
+class DatasetFacetPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
+    plugins.implements(p.IDatasetForm)
+
+    def create_package_schema(self):
+        # let's grab the default schema in our plugin
+        schema = super(DatasetFacetPlugin, self).create_package_schema()
+        # our custom field
+        schema.update({
+            'Category': [toolkit.get_validator('ignore_missing'),
+                        toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def update_package_schema(self):
+        # let's grab the default schema in our plugin
+        schema = super(DatasetFacetPlugin, self).create_package_schema()
+        # our custom field
+        schema.update({
+            'Category': [toolkit.get_validator('ignore_missing'),
+                        toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def show_package_schema(self):
+        schema = super(DatasetFacetPlugin, self).show_package_schema()
+        schema.update({
+            'custom_text': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
